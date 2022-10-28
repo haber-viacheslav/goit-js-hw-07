cb8dc466bc4cd2e740b87798962781ebc29bec1c
Viacheslav Haber <gaberslavik@gmail.com>
2022-10-28 16:36:29 +0300

    finished task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,13 +7,24 @@ const refs = {
 function addgaleryItemsMarkup(itemsArr) {
 	return itemsArr
 		.map(({ original, preview, description }) => {
-			return `<a class="gallery__item href="${original}">
-		<img class="gallery__image" src="${preview}" alt="${description}"/></a> `;
+			return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a>`;
 		})
 		.join('');
 }
+
+console.log(addgaleryItemsMarkup(galleryItems));
 
 refs.galleryRef.insertAdjacentHTML(
 	'beforeend',
 	addgaleryItemsMarkup(galleryItems)
 );
+
+const galerySimpleLightBox = new SimpleLightbox('.gallery a', {
+	captionsData: 'alt',
+	loop: true,
+	captionDelay: 250,
+	animationSpeed: 250,
+	animationSlide: true,
+});
+
+galerySimpleLightBox.show('show.simplelightbox', function () {});
